d4b9a35357b29e713050a5acb1d2fd99cf9531a5
IvanCastro <kcmtha@gmail.com>
2022-11-22 17:35:23 -0600

    chore: params and queries

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const { route } = require('../app');
+const router = express.Router();
+const package = require('../package.json')
+const time = Date.now();
+const now = new Date(time);
 /* GET home page. */
-router.get('/', (req, res, next) =>res.send({ name: "Express with SQL"}));
+router.get('/', (req, res, next) =>res.send({ name: package.name, date: now.toString()}));
 
-/* GET health page. */
-router.get('/health', (req, res, next) =>res.send({ status: "OK"}));
+//Url con parametros
+router.get("/users/:name/:id", (req, res, next) =>{
+res.send({name : req.params.name, id: req.params.id});
+});
 
+//Url con query string 
+router.get("/list", (req, res, next) =>{
+  let numbers = [1, 11 , 666, 777, 888, 999, 423];
+  res.send({number : req.query.sort ? numbers.sort() : numbers});
+});
 
 module.exports = router;
