9541d3802ec227ec7500e903c216bf83001c9a10
IvanCastro <kcmtha@gmail.com>
2022-11-22 15:51:39 -0600

    feat: add health and not found routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({ server: 'Express with SQL' });
-});
+router.get('/', (req, res, next) =>res.send({ name: "Express with SQL"}));
+
+/* GET health page. */
+router.get('/health', (req, res, next) =>res.send({ status: "OK"}));
+
 
 module.exports = router;
